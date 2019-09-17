520d85bb7b790a01964cf944e4d35c551b1185d6
Zemke <florian@zemke.io>
2019-09-17 19:44:04 +0200

    Check secret after help

--- index.js
+++ index.js
@@ -21,11 +21,6 @@ const secret = process.env.TWITCH_WEBHOOK_SECRET;
 const streams = assert(() => JSON.parse(fs.readFileSync(cacheFilePath).toString()), []);
 const hostname = assert(() => args.match(/\|--host (?:https?:\/\/)(.+?)\/?\|/)[1], 'localhost:9999');
 
-if (!secret && verifySignature) {
-  console.error('Please provide a secret via environment variable: TWITCH_WEBHOOK_SECRET');
-  process.exit(1);
-}
-
 if (help) {
   console.info(`
     ${bold('CACHE')}
@@ -45,6 +40,11 @@ if (help) {
   `);
 
   process.exit(0);
+}
+
+if (!secret && verifySignature) {
+  console.error('Please provide a secret via environment variable: TWITCH_WEBHOOK_SECRET');
+  process.exit(1);
 }
 
 const eventEmitter = new EventEmitter();
